b875a541d8b791022e0ec99d7e775bebdef1b77f
Mihai-Andrei <andrei.mihai2@student.upt.ro>
2021-05-01 22:48:00 +0300

    L8

---
+++ L8/L3.c
@@ -0,0 +1,37 @@
+#include <stdio.h>
+#include <stdlib.h>
+#define Citire(x) scanf("%d",&x)
+#define new_line printf("\n")
+int N;
+
+void Secventa(int N)
+{
+	if (N == 1)
+		printf("%d", N);
+	else
+	{
+		if (N % 2 == 0)
+		{
+			printf("%d->", N);
+			Secventa(N / 2);
+			
+		}
+		if (N % 2 == 1)
+		{
+			printf("%d->", N);
+			Secventa(3 * N + 1);
+		}
+	}
+}
+
+int main()
+{
+	printf("N=");
+	Citire(N);
+
+	Secventa(N);
+
+	new_line;
+	
+	return 0;
+}
